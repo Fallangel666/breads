f2401399867e4dcae8e86de589d08e5bd2b124d8
Fallanagel <havemedance@hotmail.com>
2022-08-03 20:54:53 -0500

    changed edit

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose');
+
+
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+);
 
-// DEPENDENCIES
-const methodOverride = require('method-override')
+// MIDDLEWARE
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
- // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
-// MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
+  res.send('Welcome to an awesome App about Breads')
+})
 
-
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
 // 404 Page
 app.get('*', (req, res) => {
@@ -35,5 +36,5 @@ app.get('*', (req, res) => {
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+  console.log('nomming at port', PORT);
 })
--- views/index.jsx
+++ views/index.jsx
@@ -1,12 +1,14 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
+
 function Index({ breads }) {
     return (
         <Default>
             <h2>Index Page</h2>
             <div className="newButton">
                 <a href="/breads/new"><button>Add a new bread</button></a>
+
             </div>
 
             {/* <p>I have {breads[0].name} bread!</p> */}
@@ -18,8 +20,8 @@ function Index({ breads }) {
 
 
 
-                            <li key={index}>
-                                <a href={`/breads/${index}`}>
+                            <li key={bread.id}>
+                                <a href={`/breads/${bread.id}`}>
                                     {bread.name}
                                 </a>
                             </li>
